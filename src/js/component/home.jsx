43421965b0e627f682a4ec2a15af814956b007c4
Sergiomerizalde <sergio.merizalde.p@gmail.com>
2022-08-25 03:03:38 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tarea, setTarea]=useState("")
+	const [todos, setTodos]=useState([])
+	function submit(e){
+		e.preventDefault()
+		setTarea(e.target.value);
+		console.log(tarea);
+		setTarea("");
+		// setTodos a lo que es set tareas con concat o push
+	}
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">Todo List</h1>
+			<form className="container" onSubmit={submit}>
+			<input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Tu correo aquí"
+			onChange={(e)=>setTarea(e.target.value)}
+			value={tarea}
+          	/>
+			</form>
 		</div>
 	);
 };
